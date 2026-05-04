2031b862eacb14f03117f297ca467e4a3ef7c6e0
maleketriki5-projects <maleketriki5@gmail.com>
2026-05-04 15:01:50 +0100

    Create projet.py
    -J'ai utilisé deux paquets " Pandas" et "Matplotlib" simultanément sous l'alias "pd" et "plt" .
    A titre d'exemple dans Pandas j'ai utilisé la fonction read_csv ( ) pour lire les donées de "Netflix movies" et les transformer en DataFrame() cad un tableau de données.
    
    ** J'ai suivi exactement les noms des variables comme "action_movies" et "netflix_df", ainsi que les labels de colonnes "release_year" et "type" donnés dans l'énoncé de DataCamp pour l'exécution de mon code.
    
    Les étapes que j'ai  suivies:
    1)Filtrer les films de type movie des années 90
    2)Filtrer les films des années 90 de type action
    3)Filtrer les films du durée courte dans une liste et les compter (utilisation du fonction len( ) 
    4)les afficher

---
+++ projet.py
@@ -0,0 +1,33 @@
+# Importer les packets pandas et  matplotlib
+import pandas as pd   #pd est l'alias de pandas
+import matplotlib.pyplot as plt   # plt est l'alias du module pyplot
+# Définitions:
+# Pandas est une bibliothèque(paquete) utilisée pour manipuler, analyser et nettoyer des données sous forme de tableaux (DataFrame).
+
+# Matplotlib est une  paquete de base en Python qui contient dans son module pyplot tous les outils pour dessiner des graphiques, des shemas pour but la visualisation
+# de données. Le code est l'outil, et le graphique est le résultat qui montre ce que les données racontent.
+
+#lecture du Netflix CSV (fichier data) comme une DataFrame
+netflix_df = pd.read_csv("netflix_data.csv")
+
+# 1. Filtrer les films des années 90 (1990 à 1999)
+movies_90 = netflix_df[(netflix_df["type"] == "Movie") & (netflix_df["release_year"] >= 1990) & (netflix_df["release_year"] < 2000)]
+
+# filtrer le data de netflix autrefois pour garder seulement "Movie" de type "Action"
+action_movies = movies_90[movies_90["genre"] == "Action"]
+
+# Filtrer les films d'action des années 90 qui durent moins de 90 minutes
+short_action_movies = action_movies[action_movies["duration"] < 90]
+
+# Compter le nombre de ces films
+short_movie_count=len(short_action_movies)
+
+# Afficher le résultat final
+print("Nombre de films d'action courts :", short_movie_count)
+
+# Afficher les 5 premiers films de ta liste filtrée
+print(short_action_movies.head())
+
+
+
+
